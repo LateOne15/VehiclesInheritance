ca51a8ed272b4fc71ef4173c58c31652f7b85867
LateOne15 <48002046+LateOne15@users.noreply.github.com>
2025-08-25 13:25:36 +0800

    Init
    Initial commit

---
+++ Inheritance/src/main/java/com/mycompany/inheritance/Engine.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.inheritance;
+
+/**
+ *
+ * @author lleyt
+ */
+public class Engine {
+    public String type;
+    public int horsepower;
+    
+    public Engine(String type, int horsepower) {
+        this.horsepower = horsepower;
+        this.type = type;
+    }
+    public void startEngine() {
+        System.out.println("Vroom Vroom!");
+    }
+    public void stopEngine() {
+        System.out.println("Vroom Disabled.");
+    }
+    
+    public String getSpecs() {
+        String specs = "The engine is a "+type+" engine, with "
+                +horsepower+" horsepower.";
+        return specs;
+    }
+}
---
+++ Inheritance/src/main/java/com/mycompany/inheritance/Plane.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.inheritance;
+
+/**
+ *
+ * @author lleyt
+ */
+public class Plane extends Vehicle{
+    public int numberOfEngines;
+    
+    public Plane(String brand, String model, int year, String engineType, 
+            int horsepower, int numberOfEngines) {
+        super(brand, model, year, engineType, horsepower);
+        this.numberOfEngines = numberOfEngines;
+        this.vehicleType = "plane";
+    }
+    
+    @Override
+    public void start() {
+        System.out.println("Plane is starting...");
+        System.out.println("Starting "+numberOfEngines+" engine"+ ((1<numberOfEngines) ? "s." : "."));
+        for (int i = 1; i < numberOfEngines+1; i++) {
+            System.out.print("Engine "+i+" started; ");
+            engine.startEngine();
+        }
+    }
+    
+    @Override
+    public void stop() {
+        System.out.println("Plane has stopped.");
+        engine.stopEngine();
+    }
+    
+    @Override
+    public String getInfo() {
+        String summary = "The "+vehicleType+" is a "+year+" "+brand+" "+model+
+                ". "+engine.getSpecs()+"\nIt has "+numberOfEngines+" engines.";
+        return summary;
+    }
+}
